cbacbdbcad62a59df9ae98e818950d6d93a70d11
mcjovial <emmanuelelias455@gmail.com>
2022-07-05 14:50:17 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,16 +1,33 @@
+import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import ResultsShowScreen from './src/screens/ResultsShowScreen';
-import SearchScreen from './src/screens/SearchScreen';
+import IndexScreen from './src/screens/IndexScreen';
+import { Provider } from './src/context/BlogContext';
+import ShowScreen from './src/screens/ShowScreen';
+import CreateScreen from './src/screens/CreateScreen';
+import EditScreen from './src/screens/EditScreen';
 
-const navigator = createStackNavigator({
-  Search: SearchScreen,
-  ResultsShow: ResultsShowScreen
-}, {
-  initialRouteName: 'Search',
-  defaultNavigationOptions: {
-    title: 'Business Search'
+const navigator = createStackNavigator(
+  {
+    Index: IndexScreen,
+    Show: ShowScreen,
+    Create: CreateScreen,
+    Edit: EditScreen,
+  },
+  {
+    initialRouteName: 'Index',
+    defaultNavigationOptions: {
+      title: 'Blogs',
+    },
   }
-})
+);
 
-export default createAppContainer(navigator)
+const App = createAppContainer(navigator);
+
+export default () => {
+  return (
+    <Provider>
+      <App />
+    </Provider>
+  );
+};
--- src/context/createDataContext.js
+++ src/context/createDataContext.js
@@ -1,7 +1,7 @@
-import React, { useReducer } from 'react';
+import React, { createContext, useReducer } from 'react';
 
 export default (reducer, actions, initialState) => {
-  const Context = React.createContext();
+  const Context = createContext();
 
   const Provider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, initialState);
