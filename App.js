79cd9ab9ac6fb71007e2ab90007f97bad02a8e58
mcjovial <emmanuelelias455@gmail.com>
2022-07-06 02:20:30 +0100

    data API sync with json server

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ const navigator = createStackNavigator(
   {
     initialRouteName: 'Index',
     defaultNavigationOptions: {
-      title: 'Blogs',
+      title: 'Blog',
     },
   }
 );
---
+++ src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "http://e7e0-197-210-226-147.ngrok.io",
+});
